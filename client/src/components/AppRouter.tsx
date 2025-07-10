38b343aa6aaf3d8ae78824e9780e05305ee901d5
AndreasNelson <nel02254@morris.umn.edu>
2025-07-10 00:13:07 -0500

    removed appshell

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AppShell } from '@mantine/core';
 import { Layout } from './Layout';
 import { HomePage } from '../pages/HomePage';
 import { SearchPage } from '../pages/SearchPage';
